cb5a7f6752f9782409a414ba59738c40dcf0e581
Elliott DuCharme <ellduc4@gmail.com>
2025-01-01 11:26:17 -0600

    Add KarmaValue property to ReactionEmote and method to set it at runtime

--- Bot/WingTechBot/Database/Models/ReactionEmote.cs
+++ Bot/WingTechBot/Database/Models/ReactionEmote.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace WingTechBot.Database.Models;
 
 ///Represents a Discord emote used for the karma and reaction tracker systems.
-public sealed class ReactionEmote(string name, ulong? emoteId) : Model
+public sealed class ReactionEmote(string name, ulong? emoteId, int karmaValue = 0) : Model
 {
 	///ID of the row.
 	[Key]
@@ -16,6 +16,9 @@ public sealed class ReactionEmote(string name, ulong? emoteId) : Model
 	///If a custom emote, the ID assigned by Discord.
 	public ulong? EmoteId { get; private init; } = emoteId;
 
+	///How much karma is given or received when a message receives this emote as a reaction.
+	public int KarmaValue { get; private set; } = karmaValue;
+
 	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 	public DateTime CreatedAt { get; private init; }
 
@@ -23,13 +26,20 @@ public sealed class ReactionEmote(string name, ulong? emoteId) : Model
 	[NotMapped]
 	public IEmote Parsed => EmoteId == null ? Emoji.Parse($":{Name}:") : Emote.Parse($"<:{Name}:{EmoteId}>");
 
+	public async Task SetKarmaValue(int newValue)
+	{
+		await using BotDbContext context = new();
+		KarmaValue = newValue;
+		await context.SaveChangesAsync();
+	}
+
 	public static async Task<ReactionEmote> Find(string name, ulong? emoteId)
 	{
 		await using BotDbContext context = new();
 		return await context.ReactionEmotes.FirstOrDefaultAsync(e => e.Name == name && e.EmoteId == emoteId);
 	}
 
-	public static async Task<ReactionEmote> AddEmote(string name, ulong? emoteId)
+	public static async Task<ReactionEmote> AddEmote(string name, ulong? emoteId, int karmaValue = 0)
 	{
 		if (String.IsNullOrWhiteSpace(name))
 			throw new ArgumentException("Invalid name");
@@ -43,7 +53,7 @@ public sealed class ReactionEmote(string name, ulong? emoteId) : Model
 		if (existing != null)
 			throw new ArgumentException("Emote exists in ReactionEmote table");
 
-		ReactionEmote emote = new(name, emoteId);
+		ReactionEmote emote = new(name, emoteId, karmaValue);
 		await context.ReactionEmotes.AddAsync(emote);
 		await context.SaveChangesAsync();
 		return emote;
