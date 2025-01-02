3cf2d3092c7acfc95c96364fbc5960b34f5f50d8
Elliott DuCharme <ellduc4@gmail.com>
2025-01-02 13:08:15 -0600

    Create ReactionSeeder

---
+++ Bot/BotTesting/Seeders/ReactionSeeder.cs
@@ -0,0 +1,43 @@
+namespace BotTesting.Seeders;
+
+///Seeds the Reactions table with values for testing.
+public static class ReactionSeeder
+{
+	private static readonly ReactionEmote[] emotes = [
+		new("upvote", 123456, 1),
+		new("downvote", 456789, -1),
+		new("thumbsup", null),
+		new("eyes", null)
+	];
+
+	public static async Task Seed(int amount, ulong startGiverId, ulong startReceiverId, int startEmoteId, ulong startMessageId)
+	{
+		ulong giverId = startGiverId;
+		ulong receiverId = startReceiverId;
+		int emoteId = startEmoteId;
+		ulong messageId = startMessageId;
+
+		//Same user giving multiple reactions to same message.
+		foreach (int i in Enumerable.Range(1, amount))
+		{
+			foreach (var emote in emotes)
+			{
+				await Reaction.AddReaction(giverId, receiverId, messageId, emote.Name, emote.DiscordEmoteId);
+			}
+
+			messageId *= 2;
+		}
+
+		//Multiple users giving the same reaction to the same message.
+		receiverId *= 2;
+		foreach (int i in Enumerable.Range(1, amount))
+		{
+			giverId++;
+
+			var emote = emotes.First();
+			await Reaction.AddReaction(giverId, receiverId, messageId, emote.Name, emote.DiscordEmoteId);
+		}
+
+		messageId *= 2;
+	}
+}
