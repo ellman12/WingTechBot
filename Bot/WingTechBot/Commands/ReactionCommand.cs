6d2e59289e59b053752912a817254e09e1bfe33d
Elliott DuCharme <ellduc4@gmail.com>
2025-01-12 14:02:11 -0600

    ReactionCommand can now send back all reactions this user got this year

--- Bot/WingTechBot/Commands/ReactionCommand.cs
+++ Bot/WingTechBot/Commands/ReactionCommand.cs
@@ -30,6 +30,30 @@ public sealed class ReactionCommand : SlashCommand
 		if (command.CommandName != "reactions")
 			return;
 
+		var options = command.Data.Options;
+
+		if (options.Count == 0)
+		{
+			await UserReactionsForYear(command);
+		}
+	}
+
+	private static async Task UserReactionsForYear(SocketSlashCommand command)
+	{
+		int year = DateTime.Now.Year;
+		var reactions = await Reaction.GetReactionsUserReceived(command.User.Id, year);
+
+		string message;
+		if (reactions.Length > 0)
+		{
+			message = $"{command.User.Username}'s reactions received for {year}\n";
+			message = reactions.Aggregate(message, (current, reaction) => current + $"* {reaction.count} {reaction.reactionEmote}\n");
+		}
+		else
+		{
+			message = $"No reactions for {year}";
+		}
 		
+		await command.FollowupAsync(message);
 	}
 }
--- Bot/WingTechBot/Database/Models/ReactionDataQueries.cs
+++ Bot/WingTechBot/Database/Models/ReactionDataQueries.cs
@@ -3,12 +3,12 @@ namespace WingTechBot.Database.Models;
 public sealed partial class Reaction
 {
 	///Gets each reaction a user has received and the amount of each.
-	public static async Task<(ReactionEmote reactionEmote, int count)[]> GetReactionsUserReceived(ulong receiverId)
+	public static async Task<(ReactionEmote reactionEmote, int count)[]> GetReactionsUserReceived(ulong receiverId, int? year = null)
 	{
 		await using BotDbContext context = new();
 		return context.Reactions
 			.Include(r => r.Emote)
-			.Where(r => r.ReceiverId == receiverId)
+			.Where(r => r.ReceiverId == receiverId && (year == null ? r.Emote.CreatedAt.Year > 0 : r.Emote.CreatedAt.Year == year))
 			.GroupBy(r => r.EmoteId)
 			.AsEnumerable()
 			.Select(g => (g.First().Emote, g.Count()))
